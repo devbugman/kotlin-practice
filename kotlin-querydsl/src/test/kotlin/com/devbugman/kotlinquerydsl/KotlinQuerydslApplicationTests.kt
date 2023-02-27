70a73bda73b8a4dec483be7c9836351eaec4dc9c
devbugman <cksqhr4961@naver.com>
2023-02-27 13:35:24 +0900

    init project

---
+++ kotlin-querydsl/src/test/kotlin/com/devbugman/kotlinquerydsl/KotlinQuerydslApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devbugman.kotlinquerydsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinQuerydslApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
