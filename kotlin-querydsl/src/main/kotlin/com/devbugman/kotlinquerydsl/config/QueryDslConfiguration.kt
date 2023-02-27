ecfcdb1e4c0acc8ea275168c9dc0aa016af74979
devbugman <cksqhr4961@naver.com>
2023-02-27 15:23:31 +0900

    setting: querydsl setting

---
+++ kotlin-querydsl/src/main/kotlin/com/devbugman/kotlinquerydsl/config/QueryDslConfiguration.kt
@@ -0,0 +1,16 @@
+package com.devbugman.kotlinquerydsl.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QueryDslConfiguration(
+    @PersistenceContext private val entityManager: EntityManager
+) {
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory = JPAQueryFactory(entityManager)
+}
